5c8b15e1c9d4c52d6720d2676ea0899ee44296a4
ImDinnerCat <mail@martin-herz.io>
2023-06-05 19:08:05 +0200

    [update] Set bot's activity

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,12 @@
+import { ActivityType } from 'discord.js'
 import { event } from '../utils'
 
 // This is the ready event
 export default event('ready', ({ log }, client) => {
+    
+    // Set the bot's activity
+    client.user?.setActivity('/info', { type: ActivityType.Playing })
+
+    // Log that the bot is ready
     log(`Logged in as ${client.user?.tag}`)
 })
