0f2f9cc492ba426aaf523d8a68d9d32c9345b25d
ImDinnerCat <mail@martin-herz.io>
2023-06-05 19:59:55 +0200

    [fix] Info text

--- src/commands/general/info.ts
+++ src/commands/general/info.ts
@@ -17,11 +17,11 @@ export default command(meta, async ({ interaction, client }) => {
     // Embed for the info command with bot profile picture as thumbnail
     const embed = new EmbedBuilder()
         .setTitle('ModCat - Discord Moderation-Bot')
-        .setDescription('Informationen über den ModCat Discord Moderation-Bot.')
+        .setDescription('Informationen über den ModCat Discord Moderations-Bot.')
         .addFields([
             {
                 name: 'Beschreibung',
-                value: `ModCat ist der hauseigene Moderation-Bot für den **${interaction.guild?.name}** Discord Server. Er entlastet die Moderatoren und hilft ihnen bei der Arbeit, in dem er automatisierte Moderationsaufgaben übernimmt. Außerdem bietet er eine Vielzahl an nützlichen Funktionen, die das Leben der Moderatoren und der Community erleichtern.`,
+                value: `ModCat ist der hauseigene Moderations-Bot für den **${interaction.guild?.name}** Discord Server. Er entlastet die Moderatoren und hilft ihnen bei der Arbeit, in dem er automatisierte Moderationsaufgaben übernimmt. Außerdem bietet er eine Vielzahl an nützlichen Funktionen, die das Leben der Moderatoren und der Community erleichtern.`,
             },
             {
                 name: 'Entwickler',
