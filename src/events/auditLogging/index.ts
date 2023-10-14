9a30a02571241d64886b3bbdf4e0a497e144d476
ImDinnerCat <mail@martin-herz.io>
2023-10-14 15:09:46 +0200

    [implement] auditlog & automod events

--- src/events/auditLogging/index.ts
+++ src/events/auditLogging/index.ts
@@ -1,8 +1,15 @@
 import { Event } from '../../types'
+import deleteMessage from './deleteMessage'
+import guildMemberBan from './guildMemberBan'
+import guildMemberUnban from './guildMemberUnban'
+import guildMemberKick from './guildMemberKick'
 
 // This is the array of events
 const events: Event<any>[] = [
-    
+    //deleteMessage,
+    guildMemberBan,
+    guildMemberUnban,
+    guildMemberKick,
 ]
 
 export default events
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,11 +1,13 @@
 import { Event } from '../types'
 import ready from './ready'
 import interactionCreate from './interactionCreate'
+import auditLogging from './auditLogging'
 import autoMod from './autoMod'
 
 // This is the array of events
 const events: Event<any>[] = [
     ...interactionCreate,
+    ...auditLogging,
     ...autoMod,
     ready,
 ]
